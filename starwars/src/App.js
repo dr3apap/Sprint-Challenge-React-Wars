d0601b50564bad47c86946db730aeaf4b1ec1bbf
Adebola  Adesina <adebolaadesina@adebolas-mbp.home>
2020-01-24 10:56:50 -0800

    component assembled

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import styled from "styled-components"
+import axios from "axios"
+import StarWarsCard from "./components/StarWarsCard"
 
 const App = () => {
+  const [people, setState] = useState([])
+
+  useEffect(() => {
+    axios.get("https://swapi.co/api/people/")
+      .then(res => {
+        setState(res.count.results)
+        console.log(res.count.results)
+
+
+      })
+
+
+      .catch(error => {
+        console.log("The data was requested", error)
+      })
+
+  }, [])
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +33,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <StarWarsCard results={people} />
     </div>
   );
 }
